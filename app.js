0272a4dd9d4afef63a31545a5ee82a8f00ec34d8
Gustavo Alexandre Nobre Mesquita <65964697+Gustavonobreza@users.noreply.github.com>
2020-11-09 18:58:33 -0300

    Testes: Responsivo - Imagem

--- app.js
+++ app.js
@@ -2,18 +2,18 @@
 
 /**
  * 1. Relacionar variáveis com as tags HTML.
- * 2. Const's de tempo abrangentes.
- * 3. Mecanismos de contagem.
- * 4. Let's dos dados do horário.
+ * 2. Mecanismos de contagem. Atualização da hora no HTML.
  */
 
- 
+
 // ! 1:
 let conteinerDisplay = document.querySelector(".conteiner");
 let sec = document.querySelector(".sec");
 let min = document.querySelector(".min");
 let hor = document.querySelector(".hor");
 let day = document.querySelector(".day");
+
+let img = document.querySelector(".img");
 
 // ! 2: 
 
@@ -27,3 +27,25 @@ function RefreshingTime() {
    day.innerHTML = time.toLocaleDateString('pt-br');
 };
 setInterval(RefreshingTime, 1000)
+
+// ! 3:
+/* Obtendo o tamanho da janela(para o responsivo) */
+
+window.addEventListener('resize', function() {
+   sizeOfThings()
+});
+
+function sizeOfThings() {
+   let windowWidth = window.innerWidth;
+   // console.clear()
+   console.log(windowWidth);
+   changeBg(windowWidth)
+};
+
+function changeBg(w) {
+   if (w < 480) {
+      img.src = './images/mobilePhoto.jpg'
+   }else{
+      img.src = './images/photo-bg.jpg'
+   }
+}
